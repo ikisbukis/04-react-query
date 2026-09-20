ff4cc7475681c5d0c4aa5b5293dffa4fa3a67ae2
Maksym Pohorielov <asassindark57@gmail.com>
2026-09-20 15:32:25 +0300

    fix main path

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import App from './App/App.tsx'
+import App from './components/App/App.tsx'
 
 const queryClient = new QueryClient();
 
@@ -12,6 +12,5 @@ createRoot(document.getElementById('root')!).render(
       <App />
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
-    
   </StrictMode>,
 )
